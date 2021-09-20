6cd1401f669adc540e0a6678242a426d38f69b75
Simon Hjorthøj <sh2@live.dk>
2021-09-21 01:13:13 +0200

    added comment to BaseService

--- Services/BaseService.cs
+++ Services/BaseService.cs
@@ -2,6 +2,7 @@
 
 namespace DatabaseConnection.Services {
    public class BaseService {
+        // We use a base service to get the DBContext in every service without writing it multiple times.
         public readonly DBContext _databaseContext;
         public BaseService(DBContext databaseContext) {
             _databaseContext = databaseContext;
